e7dc5b91b244014f24dc8d45dc65c3fb0558ba59
Pradip Kumar <pradipy2k5@gmail.com>
2025-10-21 16:20:36 +1100

    code fix

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, UploadFile, File
 import fitz
 
-
 app = FastAPI()
 
 @app.get("/")
@@ -10,12 +9,14 @@ def home():
 
 @app.post("/analyze")
 async def analyze_resume(file: UploadFile = File(...)):
-    text: str = ""
+    text = ""
 
     if file.filename.endswith(".pdf"):
-        doc = fitz.open(stream=await file.read(), filetype="pdf")
+        file_content = await file.read()
+        doc = fitz.open(stream=file_content, filetype="pdf")
         for page in doc:
             text += page.get_text()
+        doc.close()
 
         # Add your analyzer logic here
     return { "file_name": file.filename, "content_length": len(text) }
