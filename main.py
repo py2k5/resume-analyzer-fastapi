7dbae4040cfeadecd8d51c57de2b20dd7f2d2748
Pradip Kumar <pradipy2k5@gmail.com>
2025-10-21 14:35:11 +1100

    inital commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import fitz
+
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Resume Analyzer API is running"}
+
+@app.post("/analyze")
+async def analyze_resume(file: UploadFile = File(...)):
+    text: str = ""
+
+    if file.filename.endswith(".pdf"):
+        doc = fitz.open(stream=await file.read(), filetype="pdf")
+        for page in doc:
+            text += page.get_text()
+
+        # Add your analyzer logic here
+    return { "file_name": file.filename, "content_length": len(text) }
+
+
+
